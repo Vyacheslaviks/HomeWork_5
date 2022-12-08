52ac50bf419c6f5cf30088cee6a9b15ca64f0644
Slava <slavanew98@mail.ru>
2022-12-08 20:05:16 +0300

    Continue to perform 2nd exercise

--- Exc_2/Program.cs
+++ Exc_2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//Тело программы
+
+Console.WriteLine($"Четных чисел в масисве: {Search_Even_Numbers(Fill_Random_Array(100, 1000))}");
+
+//Методы
+int[] Fill_Random_Array(int min, int max)
+{
+    Random rand = new Random();
+    int[] array = new int[10];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(min, max);
+    }
+
+    string print_array = string.Join(", ", array);
+    Console.WriteLine($"[{print_array}]");
+
+    return array;
+}
+
+int Search_Even_Numbers(int[] array)
+{
+    int count = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
